2bf72ddabb6839e055e5cc5c3baf81389493aa27
GVENKATANARENDRA <103510399+GVenkat-136@users.noreply.github.com>
2023-07-30 19:47:05 +0530

    database connection init

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,46 @@
+require('dotenv').config()
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
-
+const { default: mongoose } = require('mongoose');
+const mongodb = require('mongoose')
+// const { MongoClient, ObjectId } = require('mongodb');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 8080;
 
 const url = process.env.DATABASE_URL
-const dbName = process.env.DATABASE
 const collectionName = process.env.COLLECTION
 
 app.use(express.json());
+app.use(cors())
 
-async function connectToMongoDB() {
-  const client = new MongoClient(url);
-  await client.connect();
-  console.log('Connected to MongoDB');
-  return client.db(dbName).collection(collectionName);
+// async function connectToMongoDB() {
+//   const client = new mongodb(url);
+//   await client.connect();
+//   console.log('Connected to MongoDB');
+//   return client.db(dbName).collection(collectionName);
+// }
+
+const connectDB = async () => {
+  try {
+    const conn = await mongodb.connect(`${url}`, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
 }
-
 app.get('/songs', async (req, res) => {
   const category = req.query.category;
   if (!category) {
     return res.status(400).json({ error: 'Category parameter is missing.' });
   }
-
-  const collection = await connectToMongoDB();
-
   try {
+    const connection = await connectDB();
+    console.log(connection)
     const query = { category: category };
-    const result = await collection.find(query).toArray();
+    const result = await mongoose.connection.collection(collectionName).find(query).toArray();
     res.json(result);
   } catch (error) {
     console.error('Error retrieving data:', error);
