e9ab2e11365876d38c187f8b3d164bc62f672029
Siddam Vamsee <31583305+ssvamsee@users.noreply.github.com>
2024-03-20 12:01:17 +0000

    change

--- devotional/src/compontents/Mainlayout/Mainlayout.jsx
+++ devotional/src/compontents/Mainlayout/Mainlayout.jsx
@@ -14,7 +14,7 @@ export default function Mainlayout() {
    const isslected = useSelector((state) => state.player.isselected)
 
    const fetchdata = async (qurey) => {
-      await fetch(`https://devotional.onrender.com/songs?category=${qurey}`, {
+      await fetch(`https://devotional.onrender.com/songs/?category=${qurey}`, {
          method: 'GET',
          mode: 'cors',
       }).then(response => response.json())
